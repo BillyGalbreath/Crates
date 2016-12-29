232f5af6b3640ae2b4976aa2b60aac4f1b9b4e9d
BillyGalbreath <Blake.Galbreath@GMail.com>
2016-12-29 00:42:59 -0600

    Removed spawner cage b.s. - Let a spawner plugin handle (SilkSpawners)

--- src/main/java/net/pl3x/bukkit/pl3xcrates/api/ItemNBT.java
+++ src/main/java/net/pl3x/bukkit/pl3xcrates/api/ItemNBT.java
@@ -1,10 +1,7 @@
 package net.pl3x.bukkit.pl3xcrates.api;
 
-import org.bukkit.entity.EntityType;
 import org.bukkit.inventory.ItemStack;
 
 public interface ItemNBT {
     ItemStack setItemNBT(ItemStack bukkitItem, String nbt, String path);
-
-    EntityType getEntityType(ItemStack bukkitItem);
 }
